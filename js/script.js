64ef26b8f8cb6c44ceece7b70ab1267ebaba5fa7
kenizaya <harveylstark@gmail.com>
2022-05-09 23:28:02 +0530

    Fix border length with box-sizing: border-box

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 function createGrid() {
     const container = document.querySelector(".container");
-    // const rows = document.querySelector(".rows");
-    // const columns = document.querySelector(".columns");
-
-
-
+    // Generate the GRID
     for (let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.classList.add('row');
@@ -14,9 +10,6 @@ function createGrid() {
         for (let j = 0; j < 16; j++) {
             const column = document.createElement("div");
             column.classList.add('column');
-            // if (i===15 && j === 15) {
-            //     column.classList.add('last-column');
-            // }
             console.log(column);
             row.appendChild(column);
         }                   
