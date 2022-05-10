627ab9e58aa8b46102d7ef74572f5d1a7d68d0ca
kenizaya <harveylstark@gmail.com>
2022-05-10 13:08:35 +0530

    Center the grid

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 function createGrid() {
     const container = document.querySelector(".container");
+
+    let n = 16;
+
     // Generate the GRID
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < n; i++) {
         const row = document.createElement("div");
         row.classList.add('row');
         console.log(row);
         container.appendChild(row);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < n; j++) {
             const column = document.createElement("div");
             column.classList.add('column');
             // console.log(column);
             row.appendChild(column);
         }                   
     }
+
+    // n = prompt("Enter the size of the grid(< 100): ");
+
+    // const rowCss = document.querySelector(".row");
+    // const columnCss = document.querySelector(".column");
 
     changeColor();
 
